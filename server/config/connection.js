3ffaf721518e5b7b6f0c31e5dc19a2baa055ec62
Maya N <nettathm@gmail.com>
2023-11-04 20:18:14 -0600

    attempting to deploy to heroku successfully

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 
+const URI= process.env.MONGO_URI
+
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
 
 module.exports = mongoose.connection;
